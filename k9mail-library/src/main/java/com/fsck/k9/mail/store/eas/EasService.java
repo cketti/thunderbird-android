fdf7b97b7e4c23b4004bcb7319a7325820f31299
cketti <cketti@gmail.com>
2015-09-27 00:39:03 +0200

    Import tests from AOSP and make them run with Robolectric
    Imported from AOSP repo, revision tagged 'android-5.1.1_r18'

--- k9mail-library/src/main/java/com/fsck/k9/mail/store/eas/EasService.java
+++ k9mail-library/src/main/java/com/fsck/k9/mail/store/eas/EasService.java
@@ -5,11 +5,17 @@ package com.fsck.k9.mail.store.eas;
  * Stub class to get a minimal version of the imported EAS code running.
  */
 public class EasService {
+    private static boolean protocolLogging;
+
     public static boolean getProtocolLogging() {
-        return true;
+        return protocolLogging;
     }
 
     public static boolean getFileLogging() {
         return false;
+    }
+
+    public static void setProtocolLogging(boolean enable) {
+        protocolLogging = enable;
     }
 }
